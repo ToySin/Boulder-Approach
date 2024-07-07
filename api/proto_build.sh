2fa1bfc1a64feb11a42e14c1f806377e0a2a890c
Toysin <toysin1203@hotmail.com>
2024-07-07 23:19:58 +0900

    initial server

--- api/proto_build.sh
+++ api/proto_build.sh
@@ -2,4 +2,4 @@ protoc boulder.proto --swift_out=.
 protoc boulder.proto --go_out=. 
 
 protoc boulder_service.proto --grpc-swift_out=.
-protoc boulder_service.proto --go-grpc_out=. 
+protoc boulder_service.proto --go_out=. --go-grpc_out=. 
